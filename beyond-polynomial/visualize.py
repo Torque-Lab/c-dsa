db513da632a86bc921ea6d637f3620c10bb6b8e1
Mathura_Kumar <mathura.kumar.tech@gmail.com>
2026-09-02 03:04:02 +0000

    tested fork

--- beyond-polynomial/visualize.py
+++ beyond-polynomial/visualize.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 n = int(sys.argv[1])
 
-graph_file = f"see_graph{n}.txt"
+graph_file = f"seed_graph{n}.txt"
 vc_file = f"vertex_cover{n}.txt"
 
 G = nx.Graph()
@@ -15,9 +15,9 @@ with open(graph_file) as f:
         num_nodes = int(parts[0])
         num_edges = int(parts[1])
 
-    for _ in range(num_edges):
-        u, v, w = map(int, f.readline().split())
-        G.add_edge(u, v, weight=w)
+        for _ in range(num_edges):
+            u, v, w = map(int, f.readline().split())
+            G.add_edge(u, v, weight=w)
 
 with open(vc_file) as f:
     cover_size = int(f.readline())
@@ -43,6 +43,7 @@ nx.draw(
 weights = nx.get_edge_attributes(G, "weight")
 nx.draw_networkx_edge_labels(G, pos, edge_labels=weights)
 
+plt.savefig(f"vertex{num_nodes}.png", format="png", dpi=300)
 plt.title(f"Nodes: {num_nodes}, Edges: {num_edges}, VC: {cover_size}")
 
 plt.show()
